444e216a160b3a4da1b92d67cb9b9551e490dc1f
TrikayCoder <91646919+TrikayCoder@users.noreply.github.com>
2022-12-07 20:45:20 +0700

    Update Interface IOrderList.java

--- src/main/java/dev/kay/Model/Interface/IOrderList.java
+++ src/main/java/dev/kay/Model/Interface/IOrderList.java
@@ -1,4 +1,35 @@
 package dev.kay.Model.Interface;
 
-public class IOrderList {
+import java.util.ArrayList;
+
+public interface IOrderList {
+
+    /**
+     * Generate new Order list
+     * @param iFoods List of food
+     */
+    public void generateOrder(ArrayList<IFood> iFoods);
+
+    /**
+     * Get all list of Order
+     * @return A list of Order
+     */
+    public ArrayList<IFood> getOrder();
+
+    /**
+     * Add Food into Order list
+     * @param iFood Food to add
+     */
+    public void addOrder(IFood iFood);
+
+    /**
+     * Remove Food from Order list
+     * @param iFood Food to remove
+     */
+    public void removeOrder(IFood iFood);
+
+    /**
+     * Clear list of order
+     */
+    public void clearOrder();
 }
---
+++ src/main/java/dev/kay/Model/Model/OrderList.java
@@ -0,0 +1,33 @@
+package dev.kay.Model.Model;
+
+import dev.kay.Model.Interface.IFood;
+import dev.kay.Model.Interface.IOrderList;
+
+import java.util.ArrayList;
+
+public class OrderList implements IOrderList {
+    @Override
+    public void generateOrder(ArrayList<IFood> iFoods) {
+
+    }
+
+    @Override
+    public ArrayList<IFood> getOrder() {
+        return null;
+    }
+
+    @Override
+    public void addOrder(IFood iFood) {
+
+    }
+
+    @Override
+    public void removeOrder(IFood iFood) {
+
+    }
+
+    @Override
+    public void clearOrder() {
+
+    }
+}
